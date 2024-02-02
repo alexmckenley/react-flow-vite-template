cdf5a6c36982346b08618242961de63c51e21a91
Alex Taylor <alexmckenley@gmail.com>
2024-02-02 14:26:02 -0800

    Install babel-plugin-syntax-hermes-parser

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      presets: [],
+      // Your plugins run before any built-in transform (eg: Fast Refresh)
+      plugins: ["babel-plugin-syntax-hermes-parser"],
+      // Use .babelrc files
+      babelrc: true,
+      // Use babel.config.js files
+      configFile: true,
+    }
+})],
 })
