2895c770891779c708891f73d650991f0dec0797
Alex Taylor <alexmckenley@gmail.com>
2024-02-02 17:02:46 -0800

    Use prettier v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import flowLogo from '/flow.svg'
-import './App.css'
+import {useState} from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import flowLogo from '/flow.svg';
+import './App.css';
 import * as React from 'react';
 
 hook useCounter(initial: number) {
   const [count, setCount] = useState(0);
   function incrementCount() {
-    setCount((count) => count + 1);
+    setCount(count => count + 1);
   }
-  return [count, incrementCount]
+  return [count, incrementCount];
 }
 
 component App() {
@@ -30,20 +30,21 @@ component App() {
         </a>
       </div>
       <h1>Vite + React + Flow</h1>
-      <h2>Powered by <a href="https://flow.org/blog" target="_blank">Component Syntax!</a></h2>
+      <h2>
+        Powered by{' '}
+        <a href="https://flow.org/blog" target="_blank">
+          Component Syntax!
+        </a>
+      </h2>
       <div className="card">
-        <button onClick={() => increment()}>
-          count is {count}
-        </button>
+        <button onClick={() => increment()}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
